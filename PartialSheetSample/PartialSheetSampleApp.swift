16ea43a0318aa382a9b7fef72ad979adcd77d193
greyder <ygee@yearz.co>
2022-05-23 15:12:21 +0800

    Initial Commit

---
+++ PartialSheetSample/PartialSheetSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PartialSheetSampleApp.swift
+//  PartialSheetSample
+//
+//  Created by Yhanco Grey Esteban on 5/23/22.
+//
+
+import SwiftUI
+
+@main
+struct PartialSheetSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
